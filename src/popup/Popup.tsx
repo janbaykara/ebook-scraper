bc102f24b32559d87e2d4f53be7296ca1a3fed96
Jan Baykara <janbaykara@gmail.com>
2018-10-26 13:34:31 +0100

    More reliable popup-background comms and more accurate UI

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import "./Popup.scss";
 import { Heading, Box, Card, Flex, Text, Button } from "rebass";
-import { getURL, getBookURL } from "../common/utils";
+import { getURL, getBookURL, getBook } from "../common/utils";
 
 interface AppProps {}
 
@@ -18,7 +18,11 @@ export default class Popup extends React.Component<AppProps, AppState> {
   }
 
   async componentDidMount() {
-    this.fetchBook();
+    try {
+      this.fetchBook();
+    } catch (e) {
+      // No book to begin with
+    }
 
     chrome.runtime.onMessage.addListener(
       (request: ScraperMessage, sender, sendResponse) => {
@@ -36,6 +40,56 @@ export default class Popup extends React.Component<AppProps, AppState> {
 
   fetchBook = async () => {
     const _url = await getURL();
+
+  fetchBook = (): Promise<Book> => {
+    console.log("fetching book");
+    return new Promise(async (resolve, reject) => {
+      try {
+        const _url = await getURL();
+
+        if (!_url) {
+          console.log("No URL");
+          return reject("Couldn't get URL for this book");
+        }
+
+        const url = getBookURL(_url);
+        let book = await getBook(url);
+
+        if (!book) {
+          console.log("No book", book);
+          book = { url, pages: [] };
+          const message: Messages.SaveBook = { action: "SaveBook", book };
+          chrome.runtime.sendMessage(message);
+        } else {
+          console.log("Received book", book);
+        }
+
+        this.setState({ book }, () => console.log("new book", book));
+
+        resolve(book);
+      } catch (e) {
+        return reject(e);
+      }
+    });
+  };
+
+  download = async () => {
+    if (!this.state.book.url) return;
+    const message: Messages.RequestDownload = {
+      action: "RequestDownload",
+      bookURL: this.state.book.url
+    };
+    await chrome.runtime.sendMessage(message);
+  };
+
+  reset = async () => {
+    if (!this.state.book.url) return;
+    const message: Messages.ClearBook = {
+      action: "ClearBook",
+      bookURL: this.state.book.url
+    };
+    await chrome.runtime.sendMessage(message, this.fetchBook);
+  };
 
     if (_url) {
       const url = getBookURL(_url);
@@ -74,11 +128,24 @@ export default class Popup extends React.Component<AppProps, AppState> {
   render() {
     return (
       <Box width={250}>
-        <Flex justifyContent='between' alignItems='center'>
+        <Flex justifyContent="between" alignItems="center">
           <Box width={1}>
             <Heading fontSize={2}>Ebook PDF creator 📖</Heading>
           </Box>
-          <Button onClick={this.reset} css={{ 'text-align': 'right', background: '#FAFAFA', 'border': '1px solid red' }} color='red' p={1}>reset</Button>
+          {this.state.book && (
+            <Button
+              onClick={this.reset}
+              css={{
+                "text-align": "right",
+                background: "#FAFAFA",
+                border: "1px solid red"
+              }}
+              color="red"
+              p={1}
+            >
+              reset
+            </Button>
+          )}
         </Flex>
         {this.state.book && (
           <>
@@ -88,6 +155,15 @@ export default class Popup extends React.Component<AppProps, AppState> {
                 <a href={this.state.book.url}>{this.state.book.url}</a>
               </Text>
             </Card>
+            {this.state.book.pages.length > 0 ? (
+              <Button onClick={this.download} css={{ width: "100%" }}>
+                💾 Download PDF ({this.state.book.pages.length} pages)
+              </Button>
+            ) : (
+              <Button disabled>
+                Pages will be collected as you navigate through the book
+              </Button>
+            )}
             <Button onClick={this.download} css={{ width: "100%" }}>
               💾 Download PDF ({this.state.book.pages.length} pages)
             </Button>
